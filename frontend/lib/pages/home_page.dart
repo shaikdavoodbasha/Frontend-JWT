f3d9336b777ef7b1556d58cdcad2142b07114316
shaikdavoodbasha <shannushaik080@gmail.com>
2026-03-14 11:30:45 +0530

    profile removed

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -35,7 +35,7 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
       end: Offset.zero,
     ).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeOut));
     
-    loadProfile();
+    // loadProfile();
   }
 
   @override
